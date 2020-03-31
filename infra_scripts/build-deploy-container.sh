d911b310bb5f15c18664f47583ae0b119a063a1c
bob <bob@pop-os.localdomain>
2020-03-31 17:22:25 +1300

    add env var to deal with secrets in portianer

--- infra_scripts/build-deploy-container.sh
+++ infra_scripts/build-deploy-container.sh
@@ -5,14 +5,6 @@ version_tag=$(git describe --abbrev=0 --tags)
 # enable buildx mode
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker login
-# build image
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mrllama12/test-discord-bot:latest -t mrllama12/test-discord-bot:$version_tag --push .
+# build image and push it up
+docker buildx build --build-arg --platform linux/amd64,linux/arm64,linux/arm/v7 -t mrllama12/test-discord-bot:latest -t mrllama12/test-discord-bot:$version_tag --push .
 
-# tag image
-
-# docker login
-# docker push mrllama12/test-discord-bot:latest
-# docker push mrllama12/test-discord-bot:$version_tag
-# push image
-# docker push --disable-content-trust 192.168.1.22:5000/test-discord-bot:latest
-# docker push --disable-content-trust 192.168.1.22:5000/test-discord-bot:$version_tag 
--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-pipenv install  --system --deploy --ignore-pipfile && python main.py
+if [ $ENV -eq "prod" ]
+then
+    export TOKEN=$(cat /run/secrets/token_discord_bot) && pipenv install  --system --deploy --ignore-pipfile && python main.py
+else
+    pipenv install  --system --deploy --ignore-pipfile && python main.py
+fi
