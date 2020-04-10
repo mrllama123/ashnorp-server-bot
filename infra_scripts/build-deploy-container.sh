b0ea990dc148ab4e1b2da3ebd9167a020e963e51
bob <bob@pop-os.localdomain>
2020-04-10 14:29:10 +1200

    remove arm64

--- infra_scripts/build-deploy-container.sh
+++ infra_scripts/build-deploy-container.sh
@@ -6,5 +6,5 @@ version_tag=$(git describe --abbrev=0 --tags)
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker login
 # build image and push it up
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mrllama12/test-discord-bot:latest -t mrllama12/test-discord-bot:$version_tag --push .
+docker buildx build --platform linux/amd64,linux/arm/v7 -t mrllama12/test-discord-bot:latest -t mrllama12/test-discord-bot:$version_tag --push .
 
