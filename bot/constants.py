e5470b4787cef9ed058cafe0565e71aa0949daa9
bob <bob@pop-os.localdomain>
2020-04-12 10:57:52 +1200

    added inital code for playing youtube clips and moved commands to cogs

---
+++ bot/constants.py
@@ -0,0 +1,20 @@
+# all constant vars i.e ffmpeg or you tube dl command options
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
