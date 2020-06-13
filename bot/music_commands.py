46684da8f6e8ff3c57c0c1954bdb96ec1daed152
Rowan Self <mrllama123@users.noreply.github.com>
2020-06-13 21:25:34 +1200

    added help descriptions (#12)
    Co-authored-by: bob <bob@pop-os.localdomain>

--- bot/music_commands.py
+++ bot/music_commands.py
@@ -8,29 +8,29 @@ from bot.ytdl_source import YTDLSource
 log = logging.getLogger()
 dirname = os.path.dirname(__file__)
 
-class Music(commands.Cog):
+class Music(commands.Cog, name="audio"):
     """
     class to hold all music commands 
     """
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.command(help="plays wop sound")
     async def wop(self, ctx):
         await self.play_audio_local(ctx.voice_client, "clearly.ogg")
 
-    @commands.command()
+    @commands.command(help="plays stonks sound")
     async def stonks(self, ctx):
         await self.play_audio_local(ctx.voice_client, "stonks.mp3")
     
-    @commands.command()
+    @commands.command(help="plays the goosebumps sounds")
     async def goosebumps(self, ctx, *args):
         if any(arg in ["woof", "dog"] for arg in args):
             await self.play_audio_local(ctx.voice_client, "Goosebumps woof.mp3", 0.4)
         else:
             await self.play_audio_local(ctx.voice_client, "Goosebumps Theme Song.mp3", 0.4)
     
-    @commands.command()
+    @commands.command(help="Joins a voice channel by join <vc name>")
     async def join(self, ctx, *, channel: VoiceChannel):
         """Joins a voice channel"""
 
@@ -38,14 +38,14 @@ class Music(commands.Cog):
             return await ctx.voice_client.move_to(channel)
         await channel.connect()
 
-    @commands.command()
+    @commands.command(help="Stops and disconnects the bot from voice")
     async def disconect(self, ctx):
         """Stops and disconnects the bot from voice"""
         if ctx.voice_client.is_playing():
             ctx.voice_client.stop()
         await ctx.voice_client.disconnect()
 
-    @commands.command()
+    @commands.command(help="Changes the player's volume by volume <0 to 100>")
     async def volume(self, ctx, volume: int):
         """Changes the player's volume"""
         if ctx.voice_client is None:
@@ -59,7 +59,7 @@ class Music(commands.Cog):
 
 
 
-    @commands.command()
+    @commands.command(help="streams a video url by stream <url of video>")
     async def stream(self, ctx, *, url):
         """
         streams a youtube clip (or anything else that youtube dl can play)
